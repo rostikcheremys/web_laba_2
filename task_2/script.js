6239e5835d43234554f45517b95fa5f97cf15750
rostik <rostikcheremys@gmail.com>
2024-09-23 23:20:29 +0300

    fix task_2

--- task_2/script.js
+++ task_2/script.js
@@ -11,6 +11,8 @@
         secondList.appendChild(newItem);
         item.parentElement.remove();
     });
+
+    sortList(secondListId);
 }
 
 function moveRightItems() {
@@ -19,4 +21,21 @@ function moveRightItems() {
 
 function moveLeftItems() {
     moveItems("list-second", "list-first");
+}
+
+function sortList(listId) {
+    const list = document.getElementById(listId);
+    const items = Array.from(list.getElementsByTagName('li'));
+
+    items.sort((a, b) => {
+        const nameA = a.querySelector('input').value.toLowerCase();
+        const nameB = b.querySelector('input').value.toLowerCase();
+        return nameA.localeCompare(nameB);
+    });
+
+    list.innerHTML = '';
+
+    items.forEach(item => {
+        list.appendChild(item);
+    });
 }
